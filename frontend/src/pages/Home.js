0c05e54814c22c859a44e72d693af2a1a60611c2
Fabrizio Sebis <info@fabriziosebis.it>
2022-12-04 15:35:39 +0100

    fixed CORS error

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+    
+    const [persone, setPersone ] = useState(null)
+
+    useEffect(()=> {
+
+        const fetchPersone = async () => {
+            const response = await fetch('/api/persone')
+            const json = await response.json()
+
+            if(response.ok) {
+                setPersone(json)
+            }
+        }
+
+        fetchPersone()
+    }, [])
+    
+    
+    
     return (
         <div className="home">
-
+            <div className="persone">
+                {persone && persone.map((persona)=> (
+                    <p key={persona._id}>{persona.name}</p>
+                )
+                )}
+            </div>
         </div>
     )
 }
